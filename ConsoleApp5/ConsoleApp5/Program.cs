c189a19fb39498dc067b1af2b745c70cbd13fa0c
Виктор Маслов <vicm4slov@yandex.ru>
2022-01-27 23:49:02 +0300

    Initial Commit

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        private const int port = 8888;
+        private const string host = "127.0.0.1";
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(host), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // подключаемся к удаленному хосту
+                socket.Connect(ipPoint);
+                Console.Write("Введите сообщение:");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                // получаем ответ
+                data = new byte[256]; // буфер для ответа
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; // количество полученных байт
+                int reuslt_count;
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Convert.ToInt32(Encoding.Unicode.GetString(data, 0, bytes)));
+                }
+                while (socket.Available > 0);
+
+                reuslt_count = Convert.ToInt32(builder.ToString());
+                Console.WriteLine("ответ сервера: " + reuslt_count);
+
+                // закрываем сокет
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
+
---
+++ maslov11/maslov11/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static Random random = new Random();
+        const int port = 8888; // порт для прослушивания подключений
+
+    
+           
+            static void Main(string[] args)
+            {
+                // получаем адреса для запуска сокета
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+                // создаем сокет
+                Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    // связываем сокет с локальной точкой, по которой будем принимать данные
+                    listenSocket.Bind(ipPoint);
+
+                    // начинаем прослушивание
+                    listenSocket.Listen(10);
+
+                    Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                    while (true)
+                    {
+                        Socket handler = listenSocket.Accept();
+                        // получаем сообщение
+                        StringBuilder builder = new StringBuilder();
+                        int bytes = 0; // количество полученных байтов
+                        byte[] data = new byte[256]; // буфер для получаемых данных
+
+                        do
+                        {
+                            bytes = handler.Receive(data);
+                            builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        }
+                        while (handler.Available > 0);
+
+                        Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+                        // отправляем ответ
+                        string message = ""+ Encoding.Unicode.GetString(data, 0, bytes).Length;
+                        data = Encoding.Unicode.GetBytes(message);
+                        handler.Send(data);
+                        // закрываем сокет
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        
+    }
+    }
